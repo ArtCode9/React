6bb92dddcecbd12ea83fb7a48fdbd93d1342c705
artcode9 <artcoode9@gmail.com>
2025-12-24 22:06:11 -0800

    js refresher module

---
+++ 01 - notes/02-Js Refresher/app.js
@@ -0,0 +1,8 @@
+// importing section 
+
+import * as fullData from './util/util.js';
+import { mes } from './util/util.js';
+import { sum as fun1 } from './util/util.js';
+
+
+console.warn(`⚠️ the sum of number : ${fun1(5,2)}`);
---
+++ 01 - notes/02-Js Refresher/util/util.js
@@ -0,0 +1,7 @@
+// exporting module section 
+
+export let mes = console.warn(`This message come from inside module section `);
+
+export function sum(a, b){
+   return a + b;
+}
