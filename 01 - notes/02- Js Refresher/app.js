15f25f56abf71465b43c384096f6916a00c7d84e
artcode9 <artcoode9@gmail.com>
2025-12-25 05:08:29 -0800

    8 type of value

--- 01 - notes/02- Js Refresher/app.js
+++ 01 - notes/02- Js Refresher/app.js
@@ -6,6 +6,6 @@ import { sum as fun1 } from './util/util.js';
 
 
 
-console.error(`Default is a :`, def);
-console.warn(fun2);
-console.warn(`⚠️ the sum of number : ${fun1(5,2)}`);
+// console.error(`Default is a :`, def);
+// console.warn(fun2);
+// console.warn(`⚠️ the sum of number : ${fun1(5,2)}`);
---
+++ 01 - notes/02- Js Refresher/value.js
@@ -0,0 +1,59 @@
+//  There are Different Types of values
+/**
+ * Primitive Types (7):::    ----> simple, atomic values
+ *  String   "Hello";
+ *  Number     2 , 34.2 , -12
+ *  Boolean    tru or false 
+ *  Null & undefined   
+ *  symbol (ES6)
+ *  bigint  
+ *                  let a = 10;          
+ *                  let b = a;  // Copy
+ * 
+ * Non-Primitive Type:::      ----->  collections, structures, behaviors
+ *  Objects  (arrays and function are objects in JS )
+ * 
+ *                  let obj1 = { x: 10 };
+ *                  let obj2 = obj1;    // reference
+ * _________________________________________________
+ * Variable are Data Containers:
+ *    🔻 variable are simply data containers where You store a value in a variable
+ *        whit a variable identifier like "myText"        
+ * _________________________________________________
+ * why use Variable ?
+ *  1️⃣. Reusability:  Store a value in a variable once and use it as often and in as many places as needed
+ *  2️⃣. Readability: Organize your code over several lines rather than cramming everything into a single line
+ * _________________________________________________
+ * identifiers must follow certain rules and recommendations:
+ * 🟠 Must not contain whitespace or special characters(except $ and _)  ✅valid:  $userName, age, user_name
+ * 🟠 may contain numbers but must not start with a number ✅valid: user3,  ue3r
+ * 🟠 Must not clash with reserved keywords  ❌invalid:  let , const
+ * 🟠 Should use camelCasing   userName , isCorrect
+ * 🟠 Should describe what the "thing" it identifier contains or does  Ex: loadData, userName
+ * ____________________________________________________
+ * variable :     let  
+ *                const  <<<<  not can be reassigned  so the value not change
+ *                var
+ * ___________________________________________________
+ * 
+ * */
+
+console.warn(`=============👇value section=============`);
+
+let userName = 'ARTCODE';
+let numberPhone = 9281272;
+let developer = true ; 
+let isLove =  undefined;
+
+let user = {
+   userName: "ARTCODE",
+   numberPhone: 92389023,
+   developer: true,
+   isLove: undefined,
+};
+
+// can access value inside the object
+console.warn(user.developer);
+
+// can change them 
+user.isLove = true;
