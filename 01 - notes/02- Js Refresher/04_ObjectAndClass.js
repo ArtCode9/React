b737a3cbc2616932a91ac86465d046599710c912
artcode9 <artcoode9@gmail.com>
2025-12-26 04:51:09 -0800

    object and class

---
+++ 01 - notes/02- Js Refresher/04_ObjectAndClass.js
@@ -0,0 +1,39 @@
+// objects can be used to group multiple value together
+/**
+ * object beside key value pairs can also
+ * can store functions, which we then typically call methods. 
+ */
+
+const userDev  = {
+   name: 'Max',   // name is property
+   age: 32,
+   greet() {
+      console.warn(`Hello`);
+      console.log(this.age); // this refer to this object
+   }
+}
+
+console.log(userDev.name); // this . is a key to accessing values in an object
+userDev.greet();
+
+
+// with class keyword you can create a blueprint that can then later be used to create the actual object
+// for class name you should start with capital first name
+
+// so this is a blueprint with  name of Users
+class Users {
+   constructor (name , age) {
+      this.name = name;
+      this.age = age;
+   }
+
+   greeting  () {
+      console.warn(`Hello Users Form class!`);
+   }
+
+}
+
+// now we create a new object based on the class above (that blueprint)
+const user1 = new Users("ARTCODE", 231);
+console.warn(user1);
+user1.greeting();
