1f1ecde98a10092593ee5673ae4f1fc3fc1dfae9
artcode9 <artcoode9@gmail.com>
2025-12-26 07:43:26 -0800

    DOM

---
+++ 01 - notes/02- Js Refresher/09_DOM.js
@@ -0,0 +1,20 @@
+/* 
+   something we'll not see a lot in this course
+   is that we'll reach out to the DOM
+   and select elements in the DOM manually
+   in javascript code to then read or change them.
+   now being able to reach out 
+   to loaded website and read manipulate it at runtime
+   is of course a strength of JS but it is also  something
+   we won't do when using React because React will do it for us
+   because we write React code in that declarative way
+   so selecting Elements, for example with querySelector  
+   to then remove from the screen
+   is not something we do a lot 
+   instead we'll use react for that .
+
+*/
+
+// DOM :::
+// const list = document.querySelector("ul");
+// list.remove();
