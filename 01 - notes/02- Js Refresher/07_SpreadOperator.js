f205dbb3d20ff78e6b3ad28c9ac390b6bf69b58d
artcode9 <artcoode9@gmail.com>
2025-12-26 07:24:53 -0800

    spread operator

---
+++ 01 - notes/02- Js Refresher/07_SpreadOperator.js
@@ -0,0 +1,25 @@
+/* 
+   the other crucial concept related to arrays and objects
+   about which you should know is the special spread operator
+*/
+
+const hobbies2 = ["sports", "cooking"];
+
+const newHobbies = ["reading"];
+
+const mergedHobbies = [ ...hobbies2, ...newHobbies];
+
+console.log(mergedHobbies);
+
+// now we can use  spread operator  ...   on object 
+const userMan = {
+   nickName: 'Decoder',
+   age: 23,
+}
+
+const extendedUser = {
+   isAdmin: true,
+   ...userMan
+}
+
+console.log(extendedUser);
