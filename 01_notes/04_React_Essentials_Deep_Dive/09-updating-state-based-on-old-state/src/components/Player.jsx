334be65288e07ea25f43814f3610afbdeb57e9c0
artcode9 <artcoode9@gmail.com>
2026-01-02 07:08:38 -0800

    updating state

--- 01_notes/04_React_Essentials_Deep_Dive/09-updating-state-based-on-old-state/src/components/Player.jsx
+++ 01_notes/04_React_Essentials_Deep_Dive/09-updating-state-based-on-old-state/src/components/Player.jsx
@@ -4,13 +4,18 @@ export default function Player({ name, symbol }) {  // function with props param
    const [ isEditing, setIsEditing] = useState(false);
 
    function handleEditClick () {
-      setIsEditing(true);
+      setIsEditing((editing) => !editing);
+      // this is simple a best way if you are updating your state based
+      // on previous state value you should use this function from here 
+      // of the state updating function.
    }
 
    let playerName = <span className="player-name">{name}</span>;
+   // let btnCaption = 'Edit';
 
    if(isEditing) {
-      playerName = <input type="text" required />
+      playerName = <input type="text" required value={name}/>
+      // btnCaption = 'Save';
    }
 
    return (
@@ -19,7 +24,7 @@ export default function Player({ name, symbol }) {  // function with props param
               {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit' }</button>
          </li>
    );
 }
