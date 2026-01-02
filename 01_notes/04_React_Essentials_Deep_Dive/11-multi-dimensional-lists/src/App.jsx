55a694a688162d99c64282cfe9016733a5d1a43b
artcode9 <artcoode9@gmail.com>
2026-01-02 11:36:34 -0800

    multi list

---
+++ 01_notes/04_React_Essentials_Deep_Dive/11-multi-dimensional-lists/src/App.jsx
@@ -0,0 +1,19 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+
+function App() {
+  return (
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player initialName="player 1" symbol="X" />         
+            <Player initialName="player 2" symbol="O" />         
+          </ol>
+          <GameBoard />
+        </div>
+        LOG
+      </main>
+    );
+}
+
+export default App;
