e88cc8c605e18566b542f470723e21fe59b93961
artcode9 <artcoode9@gmail.com>
2026-01-07 07:36:44 -0800

    mindmap add so important

--- 01_notes/06_Styling_React_Components/05-tailwind-finished/src/App.jsx
+++ 01_notes/06_Styling_React_Components/05-tailwind-finished/src/App.jsx
@@ -7,6 +7,11 @@ export default function App() {
       <Header />
       <main>
         <AuthInputs />
+        <div className="min-h-screen bg-cyan-700 flex justify-center items-center">
+      <h1 className="text-5xl text-white font-bold">
+        Tailwind v4 Is Working! 💪
+      </h1>
+      </div>
       </main>
     </>
   );
