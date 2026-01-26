6a5b5e889933bfd388edd6f95cfbdad1d0e0feff
artcode9 <artcoode9@gmail.com>
2026-01-26 04:41:06 -0800

    useImperativeHandle

---
+++ 01_notes/08_Working_with_Refs_and_Portals/09-Exposing-Component-APIs-via-the-useImperativeHandle-Hook/src/components/TimerChallenge.jsx
@@ -0,0 +1,50 @@
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal.jsx";
+
+ export default function TimerChallenge({ title, targetTime }) {
+   
+   const timer = useRef();
+   const dialog = useRef();
+
+   const [timerStarted, setTimerStarted] = useState(false);
+   const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+
+         timer.current =  setTimeout(() => {
+            setTimerExpired(true);
+            // dialog.current.showModal();  // this showModal method has no related to react and it's all about the HTML
+            dialog.current.open();
+         }, targetTime * 1000);
+
+         setTimerStarted(true);
+    }
+
+    function handleStop() {
+      // how we can access setTimeout function inside the handleStart function that is where ref can help us
+      // with js we can use this to stop timer :: clearTimeout(); with useRef > timer.current
+      clearTimeout(timer.current);
+    }
+
+
+   return (
+   <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="Lost" />
+      <section className="challenge">
+         <h2>{ title }</h2>
+         
+         <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+         </p>
+         <p>
+            <button onClick={timerStarted ? handleStop : handleStart}>
+               {timerStarted ? 'Stop' : 'Start'} Challenge
+            </button>
+         </p>
+         <p className={timerStarted ? 'active' : undefined}>
+         {timerStarted ? 'Time is running...' : 'Timer inactive'}
+         </p>
+      </section>
+   </>
+   );
+ }
