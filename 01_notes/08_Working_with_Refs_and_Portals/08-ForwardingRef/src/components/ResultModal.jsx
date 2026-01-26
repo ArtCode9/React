4b6c67a6ed87888bdaa5c98ce54ad948bbf30f5d
artcode9 <artcoode9@gmail.com>
2026-01-26 01:15:19 -0800

    forwardRef

---
+++ 01_notes/08_Working_with_Refs_and_Portals/08-ForwardingRef/src/components/ResultModal.jsx
@@ -0,0 +1,26 @@
+import { forwardRef} from 'react';
+
+
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime}, ref) {
+   return (
+      <dialog ref={ref} className="result-modal">
+         <h2>You {result}</h2>
+         <p>
+            The target time was <strong>{targetTime} seconds.</strong>
+         </p>
+         <p>
+            You stopped the timer with <strong>X seconds left.</strong>
+         </p>
+         <form method="dialog">
+            <button>Close</button>    
+         </form>
+      </dialog>
+   );
+});
+
+export default ResultModal;
+
+// The open after className="result-modal" make visible this dialog it is open attribute
+// The open delete after add forwardRef  from react and if i did not delete it all the modal belong to all 
+//  section open before i interact with anything
