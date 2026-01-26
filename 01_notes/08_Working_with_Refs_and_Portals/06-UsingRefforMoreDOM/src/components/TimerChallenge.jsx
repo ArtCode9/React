2e3d4690981088cbe6d91df2b49a4c82cc2b32ab
artcode9 <artcoode9@gmail.com>
2026-01-26 00:27:54 -0800

    useRef

---
+++ 01_notes/08_Working_with_Refs_and_Portals/06-UsingRefforMoreDOM/src/components/TimerChallenge.jsx
@@ -0,0 +1,41 @@
+ import { useState, useRef } from "react";
+
+ export default function TimerChallenge({ title, targetTime }) {
+   
+   const timer = useRef();
+
+   const [timerStarted, setTimerStarted] = useState(false);
+   const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+
+         timer.current =  setTimeout(() => {
+            setTimerExpired(true);
+         }, targetTime * 1000);
+
+         setTimerStarted(true);
+    }
+
+    function handleStop() {
+      // how we can access setTimeout function inside the handleStart function that is where ref can help us
+      // with js we can use this to stop timer :: clearTimeout(); with useRef > timer.current
+      clearTimeout(timer.current);
+    }
+
+
+   return <section className="challenge">
+      <h2>{ title }</h2>
+      {timerExpired && <p>You Lost!</p>}
+      <p className="challenge-time">
+         {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+         <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+         </button>
+      </p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
+   </section>
+ }
