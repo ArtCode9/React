a0a697a069030e9203708eec43bcfe5e9438e70a
artcode9 <artcoode9@gmail.com>
2026-01-30 00:50:59 -0800

    delete action

--- 01_notes/09_Practice_Project_Project_Management_App_(with_Components_State_Refs_and_More)/01-starting-project/src/App.jsx
+++ 01_notes/09_Practice_Project_Project_Management_App_(with_Components_State_Refs_and_More)/01-starting-project/src/App.jsx
@@ -21,7 +21,6 @@ function App() {
     });
   };
 
-
   function handleStartAddProject() {
     setProjectsState(prevState => {
       return {
@@ -56,11 +55,23 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectState.projects.find(
     project => project.id === projectState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject}/>;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if(projectState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
