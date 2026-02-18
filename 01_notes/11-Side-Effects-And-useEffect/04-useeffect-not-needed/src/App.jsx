bd409d97f42843d35a375c69253a4ecf726d0147
artcode9 <artcoode9@gmail.com>
2026-02-18 05:05:21 -0800

    modal useEffect dependenci

--- 01_notes/11-Side-Effects-And-useEffect/04-useeffect-not-needed/src/App.jsx
+++ 01_notes/11-Side-Effects-And-useEffect/04-useeffect-not-needed/src/App.jsx
@@ -13,8 +13,8 @@ const storedPlaces = storedIds.map((id) =>
 );
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIdOpen, setModalIsOpen] =useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -44,12 +44,12 @@ function App() {
   }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -74,7 +74,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem(
@@ -85,7 +85,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIdOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- 01_notes/11-Side-Effects-And-useEffect/04-useeffect-not-needed/src/components/Modal.jsx
+++ 01_notes/11-Side-Effects-And-useEffect/04-useeffect-not-needed/src/components/Modal.jsx
@@ -1,22 +1,23 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef,  useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+const Modal = forwardRef(function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
+   useEffect(() => {
+
+    if(open) {
         dialog.current.showModal();
-      },
-      close: () => {
+    } else {
         dialog.current.close();
-      },
-    };
-  });
+    }
+
+   }, []);
+
+   
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} >
       {children}
     </dialog>,
     document.getElementById('modal')
