1d5224d7510896d456149f89c23e44fa32ac0705
artcode9 <artcoode9@gmail.com>
2026-04-27 07:31:29 -0700

    add footer

---
+++ 01_notes/12_Practice_Project_Building_a_Quiz_App/02_Module_Introduction/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer () {
+   return (
+      <>
+         <hr/>
+         <hr/>
+         <footer>
+            <h3>This is a footer add to the App Quiz</h3>
+         </footer>
+      </>
+   )
+}
