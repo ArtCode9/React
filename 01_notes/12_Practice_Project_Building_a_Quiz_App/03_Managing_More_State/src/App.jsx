9155d5e0ca7a969c8be217c59d721b0679a8128a
artcode9 <artcoode9@gmail.com>
2026-05-08 02:28:45 -0700

    random correct answer

---
+++ 01_notes/12_Practice_Project_Building_a_Quiz_App/03_Managing_More_State/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+   return (
+   <>
+      <Header />
+      <main>
+         <Quiz />
+      </main>
+      <Footer /> 
+   </>
+   )   
+}
+
+export default App;
